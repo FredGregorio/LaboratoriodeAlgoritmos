b28142b82fb6499aef4e35aed3a3870f6279b00a
Frederico Gregório <fredd.gregorio@gmail.com>
2025-04-22 20:49:29 -0300

    aatualização

--- Lab de Algoritmos e Estrutura de Dados/Unidade 1/CodigoInicial.c
+++ Lab de Algoritmos e Estrutura de Dados/Unidade 1/CodigoInicial.c
@@ -1,8 +1,10 @@
-int x = 10;
-int *ptr = &x; // &x pega o endereço de memória de x
+void incrementa(int *num) {
+    (*num)++; // incrementa o valor apontado
+}
 
-printf("Valor de x: %d\n", x);
-printf("Endereço de x: %p\n", &x);
-printf("Conteúdo de ptr (endereço): %p\n", ptr);
-printf("Valor apontado por ptr: %d\n", *ptr); // *ptr acessa o valor no endereço
+int main() {
+    int a = 5;
+    incrementa(&a); // passa o endereço de a
+    printf("Valor de a: %d\n", a); // agora a vale 6
+}
 
