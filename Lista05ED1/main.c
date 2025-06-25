0effcfaaae3a42b063b53dc9350137d020ba9e34
Frederico Gregório <fred.engcomp@gmail.com>
2025-06-24 21:53:43 -0300

    atualziacao

--- Lista05ED1/main.c
+++ Lista05ED1/main.c
@@ -12,8 +12,39 @@ int main (){
    printf("2 - ARRAY 100.000 ELEMENTOS \n");
    printf("3 - ARRAY 1.000.000 ELEMENTOS \n");
    printf("4 - ARRAY de CARACTERES \n");
+   printf("Opcao: ");
    scanf ("%d", &tipoteste);
 
+   switch (tipoteste) {
+
+      case 1:
+
+        printf("Teste Realizado com ARRAY 1.000 ELEMENTOS \n");
+      
+      break;
+      
+      case 2:
+
+         printf("Teste Realizado com ARRAY 100.000 ELEMENTOS \n");
+      
+      break;
+
+      case 3:
+      
+      printf("Teste Realizado com ARRAY de 1.000.000 ELEMENTOS \n");
+
+      break;
+
+      case 4:
+
+      printf("Teste Realizado com ARRAY de CARACTERES \n");
+
+      
+      break;
+   
+
+
+   }
 
 
 
