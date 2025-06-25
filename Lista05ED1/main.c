edd18f0386520c7260aaee601fbdc18a268849e6
Frederico Gregório <fred.engcomp@gmail.com>
2025-06-24 21:48:01 -0300

    atualização

--- Lista05ED1/main.c
+++ Lista05ED1/main.c
@@ -5,6 +5,17 @@
 
 int main (){
 
+   int tipoteste;
+
+   printf("Como voce quer testar: \n");
+   printf("1 - ARRAY 1.000 ELEMENTOS \n");
+   printf("2 - ARRAY 100.000 ELEMENTOS \n");
+   printf("3 - ARRAY 1.000.000 ELEMENTOS \n");
+   printf("4 - ARRAY de CARACTERES \n");
+   scanf ("%d", &tipoteste);
+
+
+
 
 
 
