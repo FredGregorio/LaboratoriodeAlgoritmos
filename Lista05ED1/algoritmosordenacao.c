9e516b2be9fff30af1ae606c0ab1bd0efe81e4b2
Frederico Gregório <fredd.gregorio@gmail.com>
2025-07-06 17:33:25 -0300

    atualiz

--- Lista05ED1/algoritmosordenacao.c
+++ Lista05ED1/algoritmosordenacao.c
@@ -32,12 +32,57 @@ void bubbleSort(int *vetor, int tamanho){
 
 
 void mergeSort(int *vetor, int tamanho){
+    int meio, i, j, k;
+    if (tamanho > 1) {
+        meio = tamanho / 2;
 
+        int esquerda[meio];
+        int direita[tamanho - meio];
+
+        for (i = 0; i < meio; i++)
+            esquerda[i] = vetor[i];
+        for (i = meio; i < tamanho; i++)
+            direita[i - meio] = vetor[i];
+
+        mergeSort(esquerda, meio);
+        mergeSort(direita, tamanho - meio);
+
+        i = 0; j = 0; k = 0;
+        while (i < meio && j < (tamanho - meio)) {
+            if (esquerda[i] <= direita[j]) {
+                vetor[k++] = esquerda[i++];
+            } else {
+                vetor[k++] = direita[j++];
+            }
+        }
+        while (i < meio) {
+            vetor[k++] = esquerda[i++];
+        }
+        while (j < (tamanho - meio)) {
+            vetor[k++] = direita[j++];
+        }
+    }
 }
 
 
-
 void quickSort(int *vetor, int tamanho){
+    if (tamanho > 1) {
+        int pivo = vetor[tamanho - 1];
+        int i = -1, aux;
 
-    
+        for (int j = 0; j < tamanho - 1; j++) {
+            if (vetor[j] < pivo) {
+                i++;
+                aux = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = aux;
+            }
+        }
+        aux = vetor[i + 1];
+        vetor[i + 1] = vetor[tamanho - 1];
+        vetor[tamanho - 1] = aux;
+
+        quickSort(vetor, i + 1);
+        quickSort(vetor + i + 2, tamanho - i - 2);
+    }
 }
--- Lista05ED1/main.c
+++ Lista05ED1/main.c
@@ -1,80 +1,180 @@
 #include <stdio.h>
 #include "algoritmosordenacao.h"
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void preencherVetor (int *vetor, int tamanho) {
+   int i;
+   srand(time(NULL));
+   for (i = 0; i < tamanho; i++) {
+        vetor[i] = rand() % 1000;
+
+}
+
+}
+
+void copiarVetor(int *origem, int *destino, int tamanho) {
+   int i;
+   for (i = 0; i < tamanho; i++) {
+      destino[i] = origem[i];
+   }
+}
+
+void imprimirVetor(int *vetor, int tamanho) {
+   int i;
+   for (i = 0; i < tamanho; i++) {
+      printf( "%d", vetor[i]);
+   }
+   printf("\n");
+}
 
 
 int main (){
-
+   int tamanho;
    int tipoteste;
 
+
+do {
    printf("Como voce quer testar: \n");
    printf("1 - ARRAY 1.000 ELEMENTOS \n");
    printf("2 - ARRAY 100.000 ELEMENTOS \n");
    printf("3 - ARRAY 1.000.000 ELEMENTOS \n");
    printf("4 - ARRAY de CARACTERES \n");
+   printf("5 - Sair\n");
    printf("Opcao: ");
    scanf ("%d", &tipoteste);
 
    switch (tipoteste) {
 
-      case 1:
-      // ARRAY DE 1.000 ELEMENTOS
-      
-      //Função para atribuir valores ao vetor
+      case 1: {
+         tamanho = 1000;
+         int *vetor_original = (int *)malloc(tamanho * sizeof(int));
+         int *vetor = (int *)malloc(tamanho * sizeof(int));
+         preencherVetor(vetor_original, tamanho);
 
+         clock_t inicio, fim;
+         double tempo_gasto;
 
+         // Bubble Sort
+         copiarVetor(vetor_original, vetor, tamanho);
+         inicio = clock();
+         bubbleSort(vetor, tamanho);
+         fim = clock();
+         tempo_gasto = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+         printf("\nBubble Sort: %f segundos \n", tempo_gasto);
 
-      //Funcão para calcular o tempo
+         // Insertion Sort
+         copiarVetor(vetor_original, vetor, tamanho);
+         inicio = clock();
+         insertionSort(vetor, tamanho);
+         fim = clock();
+         tempo_gasto = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+         printf("\nInsertionSort: %f segundos \n", tempo_gasto);
 
+         // Merge Sort
+         copiarVetor(vetor_original, vetor, tamanho);
+         inicio = clock();
+         mergeSort(vetor, tamanho);
+         fim = clock();
+         tempo_gasto = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+         printf("\nmergeSort: %f segundos \n", tempo_gasto);
 
+         // Quick Sort
+         copiarVetor(vetor_original, vetor, tamanho);
+         inicio = clock();
+         quickSort(vetor, tamanho);
+         fim = clock();
+         tempo_gasto = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+         printf("\nQuickSort: %f segundos \n", tempo_gasto);
 
-      //Função Para calcular o tempo de execução dos algoritmos de ordenação
+         printf("\nTeste Realizado com ARRAY 1.000 ELEMENTOS\n");
+         printf("TESTE FINALIZADO\n");
 
-
-
-
-
-
-
-
-      printf("Teste Realizado com ARRAY 1.000 ELEMENTOS \n");
-      printf("TESTE FINALIZADO\n");
-
-      break;
+         free(vetor_original);
+         free(vetor);
+         break;
+      }
       
       case 2:
       // ARRAY DE 100.000 ELEMENTOS
+      tamanho = 100000;
 
+      
+         clock_t inicio, fim;
+         double tempo_gasto;
 
+      int *vetor_original = (int *)malloc(tamanho *sizeof(int));
+      int *vetor = (int *)malloc(tamanho * sizeof(int));
 
+      preencherVetor (vetor_original, tamanho);
 
+      //Bubble Sort
+      copiarVetor(vetor_original, vetor, tamanho);
+      inicio = clock();
+      bubbleSort(vetor,tamanho);
+      fim = clock();
+      tempo_gasto = ((double)(fim - inicio))/ CLOCKS_PER_SEC;
+      printf("\nBubble Sort: %f segundos \n", tempo_gasto);
 
+      // insertionSort
+      inicio = clock();
+      insertionSort(vetor,tamanho);
+      fim = clock();
+      tempo_gasto = ((double)(fim - inicio))/ CLOCKS_PER_SEC;
+      printf("\nInsertionSort: %f segundos \n", tempo_gasto);
 
+      // MergeSort
+      inicio = clock();
+      mergeSort(vetor,tamanho);
+      fim = clock();
+      tempo_gasto = ((double)(fim - inicio))/ CLOCKS_PER_SEC;
+      printf("\nmergeSort: %f segundos \n", tempo_gasto);
 
-
-
-
+      // QuickSort
+      inicio = clock();
+      quickSort(vetor,tamanho);
+      fim = clock();
+      tempo_gasto = ((double)(fim - inicio))/ CLOCKS_PER_SEC;
+      printf("\nQuickSort: %f segundos \n", tempo_gasto);
 
       printf("Teste Realizado com ARRAY 100.000 ELEMENTOS \n");
       printf("TESTE FINALIZADO\n");
       
       break;
 
-      case 3:
-      // ARRAY DE 1.000.000 ELEMENTOS
+      case 3: {
+         // ARRAY DE 1.000.000 ELEMENTOS
+         tamanho = 1000000;
+         int *vetor_original = (int *)malloc(tamanho * sizeof(int));
+         int *vetor = (int *)malloc(tamanho * sizeof(int));
+         preencherVetor(vetor_original, tamanho);
 
+         clock_t inicio, fim;
+         double tempo_gasto;
 
+         // MergeSort
+         copiarVetor(vetor_original, vetor, tamanho);
+         inicio = clock();
+         mergeSort(vetor, tamanho);
+         fim = clock();
+         tempo_gasto = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+         printf("\nmergeSort: %f segundos \n", tempo_gasto);
 
+         // QuickSort
+         copiarVetor(vetor_original, vetor, tamanho);
+         inicio = clock();
+         quickSort(vetor, tamanho);
+         fim = clock();
+         tempo_gasto = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+         printf("\nQuickSort: %f segundos \n", tempo_gasto);
 
+         printf("Teste Realizado com ARRAY de 1.000.000 ELEMENTOS \n");
+         printf("TESTE FINALIZADO\n");
 
-
-
-
-      
-      printf("Teste Realizado com ARRAY de 1.000.000 ELEMENTOS \n");
-      printf("TESTE FINALIZADO\n");
-
-      break;
+         free(vetor_original);
+         free(vetor);
+         break;
+      }
 
       case 4:
       //ARRAY DE CARACTERES
@@ -96,7 +196,7 @@ int main (){
 
 
    }
-
+} while (tipoteste != 5);
 
 
 
